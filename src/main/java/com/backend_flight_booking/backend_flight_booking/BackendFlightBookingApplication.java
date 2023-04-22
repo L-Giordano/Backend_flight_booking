132cdcb4c273a2d7aea8f660b05bf7ee397f2ff8
Lucas Giordano <lucas@accessin.net>
2023-04-22 17:52:21 -0300

    adding github actions

---
+++ src/main/java/com/backend_flight_booking/backend_flight_booking/BackendFlightBookingApplication.java
@@ -0,0 +1,13 @@
+package com.backend_flight_booking.backend_flight_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFlightBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFlightBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend_flight_booking/backend_flight_booking/BackendFlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_flight_booking.backend_flight_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
