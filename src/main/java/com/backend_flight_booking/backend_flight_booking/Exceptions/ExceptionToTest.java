079f4fbc6fa470fd93e7f644de60a3d58e3e2e48
Lucas Giordano <63258986+L-Giordano@users.noreply.github.com>
2023-04-23 12:14:53 -0300

    user controller added (#3)
    Co-authored-by: Lucas Giordano

---
+++ src/main/java/com/backend_flight_booking/backend_flight_booking/Exceptions/ExceptionToTest.java
@@ -0,0 +1,7 @@
+package com.backend_flight_booking.backend_flight_booking.Exceptions;
+
+public class ExceptionToTest extends Exception {
+    public ExceptionToTest  (String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/main/java/com/backend_flight_booking/backend_flight_booking/Users/UserService.java
+++ src/main/java/com/backend_flight_booking/backend_flight_booking/Users/UserService.java
@@ -1,10 +1,14 @@
 package com.backend_flight_booking.backend_flight_booking.Users;
 
-import org.junit.runner.RunWith;
+import com.backend_flight_booking.backend_flight_booking.Exceptions.UserIdNotFoundException;
+import com.backend_flight_booking.backend_flight_booking.Users.DTO.UserBasicInfoDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 
 public class UserService {
@@ -12,6 +16,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Transactional
     public URI createUser(UserModel userModel){
         userModel.setStatus(Boolean.TRUE);
         userRepository.save(userModel);
@@ -20,4 +28,37 @@ public class UserService {
                 .buildAndExpand(userModel.getId())
                 .toUri();
     }
+
+    public UserBasicInfoDTO getActiveUserById(Long id) throws UserIdNotFoundException {
+        Optional<UserModel> userOptional = userRepository.findById(id);
+        if(userOptional.isEmpty()){
+            throw new UserIdNotFoundException(String.format("User Id %s not Found", id));
+        }
+        return modelMapper.map(userOptional.get(), UserBasicInfoDTO.class);
+    }
+
+    @Transactional
+    public void updateUser(UserModel userModel) throws UserIdNotFoundException {
+        Optional<UserModel> response = userRepository.findById(userModel.getId());
+        if(response.isEmpty()){
+            throw new UserIdNotFoundException(String.format("User Id %s not Found", userModel.getId()));
+        }
+        userRepository.save(userModel);
+    }
+
+
+    @Transactional
+    public void deleteUser(Long id) throws UserIdNotFoundException {
+        Optional<UserModel> responseOptional = userRepository.findById(id);
+        if(responseOptional.isEmpty()){
+            throw new UserIdNotFoundException(String.format("User Id %s not Found", id));
+        }
+        UserModel userModel = responseOptional.get();
+        //TODO:It might be a good idea to check if the user has active bookings before deactivate it
+        userModel.setStatus(Boolean.FALSE);
+        userRepository.save(userModel);
+    }
+
+
+
 }
